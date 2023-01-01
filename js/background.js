a5b1d0189abcbf43a6a72d27dc19a41ecd23479f
AnLiuChair <3120652779@qq.com>
2023-01-01 14:44:13 +0800

    Site updated: 2023-01-01 14:44:12

--- js/background.js
+++ js/background.js
@@ -1,27 +1,8 @@
-}
-
-#footer {
-
-background: #00000000;
-
-}
-
-body{
-
-background-color:#adc3db;
-
-}
-
-#page-header {background-color:#0000}
-
-::-webkit-scrollbar-track {
-
-background-color: #ccd3db;
-
-}
-
-::-webkit-scrollbar-thumb {
-
-background-color: #6d718c;
-
-}
+<p>������������"config"�е�"inject"������</p>
+```YML
+inject:
+  head:
+  # - <link rel="stylesheet" href="/xxx.css">
+  bottom:
+    # - <script src="xxxx"></script>
+      - <script src="/js/background.js"></script>
