ac5592101a65d76fd116b0f6e2928927a27c87e2
AnLiuChair <3120652779@qq.com>
2023-01-01 14:38:40 +0800

    Site updated: 2023-01-01 14:38:40

---
+++ js/background.js
@@ -0,0 +1,27 @@
+}
+
+#footer {
+
+background: #00000000;
+
+}
+
+body{
+
+background-color:#adc3db;
+
+}
+
+#page-header {background-color:#0000}
+
+::-webkit-scrollbar-track {
+
+background-color: #ccd3db;
+
+}
+
+::-webkit-scrollbar-thumb {
+
+background-color: #6d718c;
+
+}
